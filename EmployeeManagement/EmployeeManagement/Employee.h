ebd5feeb9628464bb7c0cc229e65124c186d89bc
Sungju Huh <102649823+Sungju-Huh@users.noreply.github.com>
2022-03-31 11:59:50 +0900

    Project Setup (#8)
    - EmployeeManagement : Core Engine
      - EmployeeManagementTest : Google Test Suite
    
    Signed-off-by: Sungju Huh <sjslush@gmail.com>

---
+++ EmployeeManagement/EmployeeManagement/Employee.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Name {
+	string firstName;
+	string lastName;
+};
+struct PhoneNum {
+	string middlePhoneNum;
+	string lastPhoneNum;
+};
+struct BirthDay {
+	string year;
+	string month;
+	string day;
+};
+
+class Employee {
+public:
+	Employee(string employeeNum, Name name, PhoneNum phoneNum, BirthDay birthday, string cl, string certi) :
+		employeeNum_(employeeNum), name_(name), phoneNum_(phoneNum), birthday_(birthday), cl_(cl), certi_(certi)
+	{}
+
+	string employeeNum_;
+	Name name_;
+	PhoneNum phoneNum_;
+	BirthDay birthday_;
+
+	string cl_;
+	string certi_;
+};
