c35adc22fc7db08485c44a05998c52a39b93d02d
Hyunho Kim <hyunho.kim@gmail.com>
2022-04-01 12:00:44 +0900

    Add input interface
    Signed-off-by: Hyunho Kim <hyunho.kim@gmail.com>

---
+++ EmployeeManagement/EmployeeManagement/Input.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <string>
+
+
+class Input {
+	using Payload = std::vector<std::string>;
+public:
+	Input(const std::string& command,
+		const std::string& firstOption,
+		const std::string& secondOption,
+		const Payload& payload
+	) : command(command), firstOption(firstOption), secondOption(secondOption), payload(payload){ }
+
+	const std::string& getCommand() const { return this->command; }
+	const std::string& getFirstOption() const { return this->firstOption; }
+	const std::string& getSecondOption() const { return this->secondOption; }
+	const Payload& getPayload() const { return this->payload; }
+private:
+	std::string command;
+	std::string firstOption;
+	std::string secondOption;
+	Payload payload;
+};
