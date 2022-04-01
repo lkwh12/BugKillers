5b553d35b3d253d06f6a14ed8104a5142a962853
Hyunho Kim <hyunho.kim@gmail.com>
2022-04-01 14:53:34 +0900

    Add empty MemoryDatabase and Test
    Signed-off-by: Hyunho Kim <hyunho.kim@gmail.com>

---
+++ EmployeeManagement/EmployeeManagementTest/test_MemoryDatabase.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "MemoryDatabase.h"
+#include "MemoryDatabase.cpp"
+
+
+TEST(MemoryDatabaseTest, AddToDatabase) {
+	MemoryDatabase db;
+	Employee e1 = { "20000000",
+					Name{"SS1", "KIM"},
+					PhoneNum{"1234", "5678"},
+					BirthDay{"20", "01", "01"},
+					"CL1",
+					"ADV" };
+	auto ret = db.insert(e1);
+	EXPECT_TRUE(true); // TODO: fix me
+}
