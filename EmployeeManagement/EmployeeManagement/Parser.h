237e418061b0e45c00b42fd4505c51b78e85c693
Sungju Huh <sjslush@gmail.com>
2022-04-01 12:11:18 +0900

    [feature] add Parser and its test code
    Signed-off-by: Sungju Huh <sjslush@gmail.com>

---
+++ EmployeeManagement/EmployeeManagement/Parser.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Parser {
+public:
+	vector<string> parse(const string& inputLine);
+};
---
+++ EmployeeManagement/EmployeeManagementTest/ParserTest.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include "../EmployeeManagement/Parser.cpp"
+
+using namespace std;
+
+TEST(TestCaseName, ParserAddTest) {
+	Parser* pParser = new Parser();
+	const string sample = "ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV";
+	vector<string> result = pParser->parse(sample);
+
+	EXPECT_STREQ(result[0].c_str(), "ADD");
+	EXPECT_STREQ(result[1].c_str(), " ");
+	EXPECT_STREQ(result[2].c_str(), " ");
+	EXPECT_STREQ(result[3].c_str(), " ");
+	EXPECT_STREQ(result[4].c_str(), "15123099");
+	EXPECT_STREQ(result[5].c_str(), "VXIHXOTH JHOP");
+	EXPECT_STREQ(result[6].c_str(), "CL3");
+	EXPECT_STREQ(result[7].c_str(), "010-3112-2609");
+	EXPECT_STREQ(result[8].c_str(), "19771211");
+	EXPECT_STREQ(result[9].c_str(), "ADV");
+	delete pParser;
+}
+
+TEST(TestCaseName, ParserModifyTest) {
+	Parser* pParser = new Parser();
+	const string sample = "MOD,-p, , ,name,FB NTAWR,birthday,20050520";
+	vector<string> result = pParser->parse(sample);
+
+	EXPECT_STREQ(result[0].c_str(), "MOD");
+	EXPECT_STREQ(result[1].c_str(), "-p");
+	EXPECT_STREQ(result[2].c_str(), " ");
+	EXPECT_STREQ(result[3].c_str(), " ");
+	EXPECT_STREQ(result[4].c_str(), "name");
+	EXPECT_STREQ(result[5].c_str(), "FB NTAWR");
+	EXPECT_STREQ(result[6].c_str(), "birthday");
+	EXPECT_STREQ(result[7].c_str(), "20050520");
+	delete pParser;
+}
+
+TEST(TestCaseName, ParserSearchTest) {
+	Parser* pParser = new Parser();
+	const string sample = "SCH,-p,-y, ,birthday,2003";
+	vector<string> result = pParser->parse(sample);
+
+	EXPECT_STREQ(result[0].c_str(), "SCH");
+	EXPECT_STREQ(result[1].c_str(), "-p");
+	EXPECT_STREQ(result[2].c_str(), "-y");
+	EXPECT_STREQ(result[3].c_str(), " ");
+	EXPECT_STREQ(result[4].c_str(), "birthday");
+	EXPECT_STREQ(result[5].c_str(), "2003");
+	delete pParser;
+}
+
+TEST(TestCaseName, ParserDeleteTest) {
+	Parser* pParser = new Parser();
+	const string sample = "DEL,-p,-l, ,name,MPOSXU";
+	vector<string> result = pParser->parse(sample);
+
+	EXPECT_STREQ(result[0].c_str(), "DEL");
+	EXPECT_STREQ(result[1].c_str(), "-p");
+	EXPECT_STREQ(result[2].c_str(), "-l");
+	EXPECT_STREQ(result[3].c_str(), " ");
+	EXPECT_STREQ(result[4].c_str(), "name");
+	EXPECT_STREQ(result[5].c_str(), "MPOSXU");
+	delete pParser;
+}
