19be6f81ce56c504190cbed2b86ff1aa9a0c3c97
Sungju Huh <sjslush@gmail.com>
2022-04-01 15:38:24 +0900

    [refactory] avoid replicated input in Employee class
    Name, PhoneNum, BirthDay 구조체에 생성자를 만들어서
    중복되는 입력을 피했습니다.
    
    Signed-off-by: Sungju Huh <sjslush@gmail.com>

--- EmployeeManagement/EmployeeManagementTest/test_MemoryDatabase.cpp
+++ EmployeeManagement/EmployeeManagementTest/test_MemoryDatabase.cpp
@@ -5,9 +5,9 @@
 TEST(MemoryDatabaseTest, AddToDatabase) {
 	MemoryDatabase db;
 	Employee e1 = { "20000000",
-					Name{"SS1 KIM", "SS1", "KIM"},
-					PhoneNum{"010-1234-5678", "1234", "5678"},
-					BirthDay{"20000101", "2000", "01", "01"},
+					Name("SS1", "KIM"),
+					PhoneNum("1234", "5678"),
+					BirthDay("2000", "01", "01"),
 					"CL1",
 					"ADV" };
 	auto ret = db.insert(e1);
