f6cf54ffd0410ea6ddb232608c4fe63c989c721a
Hyunho Kim <hyunho.kim@gmail.com>
2022-04-04 13:11:58 +0900

    [test] add missing test cases for db query
    Signed-off-by: Hyunho Kim <hyunho.kim@gmail.com>

--- EmployeeManagement/EmployeeManagementTest/MemoryDatabaseTest.cpp
+++ EmployeeManagement/EmployeeManagementTest/MemoryDatabaseTest.cpp
@@ -51,7 +51,7 @@ TEST(MemoryDatabaseTest, Query) {
 					"PRO" };
 	Employee e3 = { "20000003",
 					Name("SS3", "KIM"),
-					PhoneNum("2234", "5678"),
+					PhoneNum("2235", "5678"),
 					BirthDay("2001", "02", "01"),
 					"CL3",
 					"PRO" };
@@ -61,31 +61,66 @@ TEST(MemoryDatabaseTest, Query) {
 
 	auto val = "20000001";
 	auto r1 = db.query(Filter(Filter::Column::EMPLOYEE_NUM, val));
-	ASSERT_EQ(1, r1.size());
+	EXPECT_EQ(1, r1.size());
 	for (auto& r : r1) EXPECT_EQ(val, r->employeeNum_);
 
 	val = "SS2";
 	r1 = db.query(Filter(Filter::Column::FIRST_NAME, val));
-	ASSERT_EQ(1, r1.size());
+	EXPECT_EQ(1, r1.size());
 	for (auto& r : r1) EXPECT_EQ(val, r->name_.firstName);
 
 	val = "KIM";
 	r1 = db.query(Filter(Filter::Column::LAST_NAME, val));
-	ASSERT_EQ(2, r1.size());
+	EXPECT_EQ(2, r1.size());
 	for (auto& r: r1) EXPECT_EQ(val, r->name_.lastName);
 
 	val = "SS1 KIM";
 	r1 = db.query(Filter(Filter::Column::NAME, val));
-	ASSERT_EQ(1, r1.size());
+	EXPECT_EQ(1, r1.size());
 	for (auto& r : r1) EXPECT_EQ(val, r->name_.name);
+
+	val = "010-2234-5678";
+	r1 = db.query(Filter(Filter::Column::TEL, val));
+	EXPECT_EQ(1, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->phoneNum_.phoneNum);
+
+	val = "5678";
+	r1 = db.query(Filter(Filter::Column::TEL_LAST, val));
+	EXPECT_EQ(3, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->phoneNum_.lastPhoneNum);
+
+	val = "2234";
+	r1 = db.query(Filter(Filter::Column::TEL_MIDDLE, val));
+	EXPECT_EQ(1, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->phoneNum_.middlePhoneNum);
+
+	val = "20010201";
+	r1 = db.query(Filter(Filter::Column::BIRTH, val));
+	EXPECT_EQ(2, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->birthday_.birthDay);
+
+	val = "2001";
+	r1 = db.query(Filter(Filter::Column::BIRTH_YEAR, val));
+	EXPECT_EQ(2, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->birthday_.year);
+
+	val = "01";
+	r1 = db.query(Filter(Filter::Column::BIRTH_MONTH, val));
+	EXPECT_EQ(1, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->birthday_.month);
+
+	val = "01";
+	r1 = db.query(Filter(Filter::Column::BIRTH_DAY, val));
+	EXPECT_EQ(3, r1.size());
+	for (auto& r : r1) EXPECT_EQ(val, r->birthday_.day);
 
 	val = "CL3";
 	r1 = db.query(Filter(Filter::Column::CL, val));
-	ASSERT_EQ(1, r1.size());
+	EXPECT_EQ(1, r1.size());
 	for (auto& r : r1) EXPECT_EQ(val, r->cl_);
 
 	val = "PRO";
 	r1 = db.query(Filter(Filter::Column::CERTI, val));
-	ASSERT_EQ(2, r1.size());
+	EXPECT_EQ(2, r1.size());
 	for (auto& r : r1) EXPECT_EQ(val, r->certi_);
 }
