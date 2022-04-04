56796c1d7bf6f030c9855cbaf82099caf7836df4
yh0203kim <michaelkim9202@gmail.com>
2022-04-04 11:42:44 +0900

    [feature] Modify Data 동작 추가

--- EmployeeManagement/EmployeeManagement/Mod.h
+++ EmployeeManagement/EmployeeManagement/Mod.h
@@ -17,8 +17,88 @@ public:
 	}
 
 	bool doModify(vector<Employee>& searchResult) {
+		vector<string> payload = input_.getPayload();
+		string modifyColumn = payload.at(2);
+
+		if (modifyColumn == "employeeNum") {
+			modifyEmployeeNum(searchResult, payload.at(3));
+		}
+		else if (modifyColumn == "name") {
+			modifyName(searchResult, payload.at(3));
+		}
+		else if (modifyColumn == "phoneNum") {
+			modifyPhoneNum(searchResult, payload.at(3));
+		}
+		else if (modifyColumn == "birthday") {
+			modifyBirthDay(searchResult, payload.at(3));
+		}
+		else if (modifyColumn == "cl") {
+			modifyCl(searchResult, payload.at(3));
+		}
+		else if (modifyColumn == "certi") {
+			modifyCerti(searchResult, payload.at(3));
+		}
+		else {
+			throw invalid_argument("ERR: Invalid Option0 Type");
+			return false;
+		}
+
 		return true;
 	}
 private:
 	Input input_;
+
+	void modifyEmployeeNum(vector<Employee>& searchResult, const string& modifyContents) {
+		for (auto& Employee : searchResult) {
+			Employee.employeeNum_ = modifyContents;
+		}
+	}
+
+	void modifyName(vector<Employee>& searchResult, const string& modifyContents) {
+		vector<string> ModifyName = splitByDelimiter(modifyContents, ' ');
+
+		for (auto& Employee : searchResult) {
+			Employee.name_ = Name(ModifyName[0], ModifyName[1]);
+		}
+	}
+
+	void modifyPhoneNum(vector<Employee>& searchResult, const string& modifyContents) {
+		vector<string> modifyPhoneNumber = splitByDelimiter(modifyContents, '-');
+
+		for (auto& Employee : searchResult) {
+			Employee.phoneNum_ = PhoneNum(modifyPhoneNumber[1], modifyPhoneNumber[2]);
+		}
+	}
+
+	void modifyBirthDay(vector<Employee>& searchResult, const string& modifyContents) {
+
+		for (auto& Employee : searchResult) {
+			Employee.birthday_ = BirthDay(modifyContents.substr(0, 2), modifyContents.substr(2, 2), modifyContents.substr(4, 2));
+		}
+	}
+
+	void modifyCl(vector<Employee>& searchResult, const string& modifyContents) {
+		for (auto& Employee : searchResult) {
+			Employee.cl_ = modifyContents;
+		}
+	}
+
+	void modifyCerti(vector<Employee>& searchResult, const string& modifyContents) {
+		for (auto& Employee : searchResult) {
+			Employee.certi_ = modifyContents;
+		}
+	}
+
+	vector<string> splitByDelimiter(string str, char Delimiter) {
+		istringstream iss(str);
+		string buffer;
+
+		vector<string> result;
+
+		while (getline(iss, buffer, Delimiter)) {
+			result.push_back(buffer);
+		}
+
+		return result;
+	}
 };
