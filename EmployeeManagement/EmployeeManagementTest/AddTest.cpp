8f1524727eaa2a7edc599fe931a04ad6889536ea
lkwh12 <lkwh12@gmail.com>
2022-04-04 12:57:10 +0900

    [feature] Develop Add Functions & Add Unit Test

--- EmployeeManagement/EmployeeManagementTest/AddTest.cpp
+++ EmployeeManagement/EmployeeManagementTest/AddTest.cpp
@@ -2,72 +2,87 @@
 #include "../EmployeeManagement/Employee.h"
 #include "../EmployeeManagement/Add.h"
 #include "../EmployeeManagement/Parser.h"
+#include "../EmployeeManagement/MemoryDatabase.h"
+#include "../EmployeeManagement/FileLogger.h"
 
-class AddTest : public ::testing::Test {
-protected:
-	void SetUp() override {
-		commands.emplace_back("ADD, , , ,15120999,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV");
-		commands.emplace_back("ADD, , , ,17112609,FB NTAWR,CL4,010-5645-6122,19861203,PRO");
-		commands.emplace_back("ADD, , , ,18115050,TTETHU HBO,CL3,010-4581-2050,20080718,ADV");
-	}
+TEST(AddTest, BasicAddTest) {
 
-	void TearDown() override {
-		commands.clear();
-	}
-
-public:
+	FileIO outputfile("../EmployeeManagement/result.txt", FileType::OUTPUT);
+	MemoryDatabase db;
+	FileLogger fileLogger(outputfile);
 	Parser parser;
-	vector<string> commands;
-	Add* addCommand;
-};
 
-TEST_F(AddTest, BasicAddTest) {
-	
-	addCommand = new Add(parser.parseLine(commands[0]));
-	//EXPECT_EQ(addCommand->execute(database, logger), 0);
+	outputfile.open();
 
-	addCommand = new Add(parser.parseLine(commands[1]));
-	//EXPECT_EQ(addCommand->execute(database, logger), 0);
+	Add addCommand1(parser.parseLine("ADD, , , ,15120999,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
+	EXPECT_EQ(addCommand1.execute(db, fileLogger), true);
 
-	addCommand = new Add(parser.parseLine(commands[2]));
-	//EXPECT_EQ(addCommand->execute(database, logger), 0);
+	Add addCommand2(parser.parseLine("ADD, , , ,17112609,FB NTAWR,CL4,010-5645-6122,19861203,PRO"));
+	EXPECT_EQ(addCommand2.execute(db, fileLogger), true);
+
+	Add addCommand3(parser.parseLine("ADD, , , ,18115050,TTETHU HBO,CL3,010-4581-2050,20080718,ADV"));
+	EXPECT_EQ(addCommand3.execute(db, fileLogger), true);
+
+	outputfile.close();
 }
 
-TEST_F(AddTest, SameEmpNoAddCase) {
+TEST(AddTest, SameEmpNoAddCase) {
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,19120999,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), 0);
-	
-	addCommand = new Add(parser.parseLine("ADD, , , ,19120999,FB NTAWR,CL4,010-5645-6122,19861203,PRO"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
-	
-	addCommand = new Add(parser.parseLine("ADD, , , ,19120999,TTETHU HBO,CL3,010-4581-2050,20080718,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	FileIO outputfile("../EmployeeManagement/result.txt", FileType::OUTPUT);
+	MemoryDatabase db;
+	FileLogger fileLogger(outputfile);
+	Parser parser;
 
-	
+	outputfile.open();
+
+	Add addCommand1(parser.parseLine("ADD, , , ,19120999,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
+	EXPECT_EQ(addCommand1.execute(db, fileLogger), true);
+
+	Add addCommand2(parser.parseLine("ADD, , , ,19120999,FB NTAWR,CL4,010-5645-6122,19861203,PRO"));
+	EXPECT_EQ(addCommand2.execute(db, fileLogger), false);
+
+	Add addCommand3(parser.parseLine("ADD, , , ,19120999,TTETHU HBO,CL3,010-4581-2050,20080718,ADV"));
+	EXPECT_EQ(addCommand3.execute(db, fileLogger), false);
+
+	outputfile.close();
 }
 
-TEST_F(AddTest, WrongValueAddCase) {
+TEST(AddTest, WrongValueAddCase) {
+	FileIO outputfile("../EmployeeManagement/result.txt", FileType::OUTPUT);
+	MemoryDatabase db;
+	FileLogger fileLogger(outputfile);
+	Parser parser;
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,15120999,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	outputfile.open();
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,17112609,FB NTAWR,CL4,010-5645-6122,19861203,PRO"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	Add addCommand1(parser.parseLine("ADD, , , ,15120999,VXIHXOTH JHOP,CL3,010-32112-2609,197471211,ADV"));
+	EXPECT_EQ(addCommand1.execute(db, fileLogger), false);
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,18115023232350,TTETHU HBO,CL3,010-2050,20080718,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	Add addCommand2(parser.parseLine("ADD, , , ,17112609,FB NTAWR,CL4,010-5645-6122,198631203,PRO"));
+	EXPECT_EQ(addCommand2.execute(db, fileLogger), false);
+
+	Add addCommand3(parser.parseLine("ADD, , , ,18115023232350,TTETHU HBO,CL3,010-2050,20080718,ADV"));
+	EXPECT_EQ(addCommand3.execute(db, fileLogger), false);
+
+	outputfile.close();
 }
 
+TEST(AddTest, NullValueAddCase) {
+	FileIO outputfile("../EmployeeManagement/result.txt", FileType::OUTPUT);
+	MemoryDatabase db;
+	FileLogger fileLogger(outputfile);
+	Parser parser;
 
-TEST_F(AddTest, NullValueAddCase) {
+	outputfile.open();
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,15120999, ,CL3,010-3112-2609,19771211,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	Add addCommand1(parser.parseLine("ADD, , , ,15120999, ,CL3,010-3112-2609,19771211,ADV"));
+	EXPECT_EQ(addCommand1.execute(db, fileLogger), false);
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,17112609,FB NTAWR,CL4, ,19861203,PRO"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	Add addCommand2(parser.parseLine("ADD, , , ,17112609,FB NTAWR,CL4, ,19861203,PRO"));
+	EXPECT_EQ(addCommand2.execute(db, fileLogger), false);
 
-	addCommand = new Add(parser.parseLine("ADD, , , ,18115050,TTETHU HBO, ,010-4581-2050,20080718,ADV"));
-	//EXPECT_EQ(addCommand->execute(database, logger), -1);
+	Add addCommand3(parser.parseLine("ADD, , , ,18115050,TTETHU HBO, ,010-4581-2050,20080718,ADV"));
+	EXPECT_EQ(addCommand3.execute(db, fileLogger), false);
+
+	outputfile.close();
 }
