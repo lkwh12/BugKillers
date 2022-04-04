dfa217f49a9b20f1a21f5ee34b0eb8cd19f5c35a
minje <minje46@gmail.com>
2022-04-04 19:28:03 +0900

    [feature] implement delete module

--- EmployeeManagement/EmployeeManagement/Del.h
+++ EmployeeManagement/EmployeeManagement/Del.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "ICommand.h"
+#include "FilterConverter.h"
 #include "Filter.h"
 #include "Input.h"
-#include "FilterConverter.h"
 
 using namespace std;
 
@@ -11,17 +11,83 @@ class Del : public ICommand {
 public:
 	Del(const Input& input_) : input(input_) {}
 	virtual bool execute(IDatabase& db, ILogger& logger) {
-		FilterConverter filterConverter;
+		// check Argument
 
-		auto outputList = db.query(filterConverter.getFilter(input));
-		logger.dump("DEL", outputList);
-		vector<string> employeeNums;
-		employeeNums.reserve(outputList.size());
-		for (auto e : outputList) employeeNums.emplace_back(e->employeeNum_);
-
-		db.remove(employeeNums);
-		return true;
+		return false;
 	}
+
+	int deleteById(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
+	int deleteByName(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
+	int deleteByPhone(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
+	int deleteByBirth(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
+	int deleteByCl(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
+	int deleteByCerti(IDatabase& db, Filter& filter)
+	{
+		auto result = db.query(filter);
+
+		vector<string> eNum;
+		for (auto it : result)
+			eNum.emplace_back(it->employeeNum_);
+		db.remove(eNum);
+
+		return result.size();
+	}
+
 private:
 	Input input;
 };
--- EmployeeManagement/EmployeeManagementTest/DeleteTest.cpp
+++ EmployeeManagement/EmployeeManagementTest/DeleteTest.cpp
@@ -1,153 +1,144 @@
 #include "pch.h"
-#include "../EmployeeManagement/Employee.h"
-#include "../EmployeeManagement/Del.h"
+#include "../EmployeeManagement/MemoryDatabase.h"
+#include "../EmployeeManagement/FileLogger.h"
 #include "../EmployeeManagement/Parser.h"
+#include "../EmployeeManagement/Del.h"
 
 using namespace std;
 
 class DeleteTest : public ::testing::Test {
 protected:
 	void SetUp() override {
-
+		this->Database.push_back({ "20000001",
+				Name("SAMSUNG", "KIM"),
+				PhoneNum("1234", "5678"),
+				BirthDay("2000", "01", "01"),
+				"CL1",
+				"ADV" });
+		this->Database.push_back({ "20000002",
+					Name("SAMSUNG", "KANG"),
+					PhoneNum("2345", "6789"),
+					BirthDay("2001", "02", "02"),
+					"CL2",
+					"PRO" });
+		this->Database.push_back({ "20000003",
+					Name("KAM", "KIM"),
+					PhoneNum("3456", "5678"),
+					BirthDay("2001", "02", "01"),
+					"CL3",
+					"EXP" });
+		this->db.insert(Database[0]);
+		this->db.insert(Database[1]);
+		this->db.insert(Database[2]);
 	}
 
 public:
+	MemoryDatabase db;
+	vector<Employee> Database;
 	Parser parser;
-	shared_ptr<Del> delCommand;
+	FilterConverter filterConverter;
 };
 
 // name, phone, birth
 TEST_F(DeleteTest, TestNoOption)
 {
 	vector<string> cmds;
-	cmds.emplace_back("DEL, , , ,id, 20000000");
-	cmds.emplace_back("DEL, , , ,name, SAMSUNG KIM");
-	cmds.emplace_back("DEL, , , ,phone, 01012345678");
-	cmds.emplace_back("DEL, , , ,birth, 20200101");
-	cmds.emplace_back("DEL, , , ,cl, CL1");
-	cmds.emplace_back("DEL, , , ,certi, PRO");
+	cmds.emplace_back("DEL, , , ,employeeNum,20000001");
+	cmds.emplace_back("DEL, , , ,cl,CL3");
+	cmds.emplace_back("DEL, , , ,certi,PRO");
 
-	// Return the number of deleted data
-	//delCommand = new Del(parser.parseLine(cmds[0]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(1, deleteById(cmd_id));
+	// DB[0] deleted
+	Del delCommand0(parser.parseLine(cmds[0]));
+	Filter filter0 = filterConverter.getFilter(parser.parseLine(cmds[0]));
+	EXPECT_EQ(1, delCommand0.deleteById(db, filter0));
 
-	//delCommand = new Del(parser.parseLine(cmds[1]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(2, deleteByName(cmd_name));
+	// DB[2] deleted
+	Del delCommand1(parser.parseLine(cmds[1]));
+	Filter filter1 = filterConverter.getFilter(parser.parseLine(cmds[1]));
+	EXPECT_EQ(1, delCommand1.deleteByCl(db, filter1));
 
-	//delCommand = new Del(parser.parseLine(cmds[2]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(4, deleteByPhone(cmd_phone));
-
-	//delCommand = new Del(parser.parseLine(cmds[3]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(8, deleteByBirth(cmd_birth));
-
-	//delCommand = new Del(parser.parseLine(cmds[4]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(16, deleteByCl(cmd_cl));
-
-	//delCommand = new Del(parser.parseLine(cmds[5]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(50, deleteByCerti(cmd_certi));
+	// DB[1] deleted
+	Del delCommand2(parser.parseLine(cmds[2]));
+	Filter filter2 = filterConverter.getFilter(parser.parseLine(cmds[2]));
+	EXPECT_EQ(1, delCommand2.deleteByCl(db, filter2));
 }
 
 // name, phone, birth
 TEST_F(DeleteTest, TestName)
 {
 	vector<string> cmds;
-	cmds.emplace_back("DEL, , , ,name, SAMSUNG KANG");
-	cmds.emplace_back("DEL,-p, , ,name, SAMSUNG KIM");
-	cmds.emplace_back("DEL,-p,-f, ,name, SAMSUNG");
-	cmds.emplace_back("DEL,-p,-l, ,name, KIM");
+	cmds.emplace_back("DEL, , , ,name,SAMSUNG KANG");
+	cmds.emplace_back("DEL, ,-l, ,name,KIM");
+	cmds.emplace_back("DEL, ,-f, ,name,SAMSUNG");
 
+	// DB[1] deleted
+	Del delCommand0(parser.parseLine(cmds[0]));
+	Filter filter0 = filterConverter.getFilter(parser.parseLine(cmds[0]));
+	EXPECT_EQ(1, delCommand0.deleteByName(db, filter0));
 
-	// Return the number of deleted data
-	//delCommand = new Del(parser.parseLine(cmds[0]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(NONE, deleteByName(cmd));
+	// DB[0], DB[2] deleted
+	Del delCommand1(parser.parseLine(cmds[1]));
+	Filter filter1 = filterConverter.getFilter(parser.parseLine(cmds[1]));
+	EXPECT_EQ(2, delCommand1.deleteByName(db, filter1));
 
-	//delCommand = new Del(parser.parseLine(cmds[1]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-//	EXPECT_EQ(1, deleteByName(cmd_n));
-
-
-	// Return the set of delete data which has parameter's name
-	//delCommand = new Del(parser.parseLine(cmds[2]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_f;
-	//	EXPECT_EQ(result_f, deleteByName(cmd_f));
-
-	//delCommand = new Del(parser.parseLine(cmds[3]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_l;
-	//	EXPECT_EQ(result_l, deleteByName(cmd_l));
-
+	// Nothing
+	Del delCommand2(parser.parseLine(cmds[2]));
+	Filter filter2 = filterConverter.getFilter(parser.parseLine(cmds[2]));
+	EXPECT_EQ(0, delCommand1.deleteByName(db, filter2));
 }
 
 TEST_F(DeleteTest, TestPhone)
 {
 	vector<string>cmds;
-	cmds.emplace_back("DEL, , , ,phone, 01098765432");
-	cmds.emplace_back("DEL, , , ,phone, 01012345678");
-	cmds.emplace_back("DEL,-p,-m, ,phone, 1234");
-	cmds.emplace_back("DEL,-p,-l, ,phone, 5678");
+	cmds.emplace_back("DEL, ,-m, ,phoneNum,2345");
+	cmds.emplace_back("DEL, ,-l, ,phoneNum,5678");
+	cmds.emplace_back("DEL, , , ,phoneNum,010-1234-5678");
 
-	// Return the number of deleted data
-	//delCommand = new Del(parser.parseLine(cmds[0]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(NONE, deleteByPhone(cmd));
+	// DB[1] deleted
+	Del delCommand0(parser.parseLine(cmds[0]));
+	Filter filter0 = filterConverter.getFilter(parser.parseLine(cmds[0]));
+	EXPECT_EQ(1, delCommand0.deleteByPhone(db, filter0));
 
-	//delCommand = new Del(parser.parseLine(cmds[1]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(3, deleteByPhone(cmd_p));
+	// DB[0], DB[2] deleted
+	Del delCommand1(parser.parseLine(cmds[1]));
+	Filter filter1 = filterConverter.getFilter(parser.parseLine(cmds[1]));
+	EXPECT_EQ(2, delCommand1.deleteByPhone(db, filter1));
 
-	// Return the set of delete data which has parameter's name
-	//delCommand = new Del(parser.parseLine(cmds[2]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_m;
-	//	EXPECT_EQ(result_m, deleteByPhone(cmd_m));
-
-	//delCommand = new Del(parser.parseLine(cmds[3]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_l;
-	//	EXPECT_EQ(result_l, deleteByPhone(cmd_l));
+	// DB[0] deleted
+	db.insert(Database[0]);
+	Del delCommand2(parser.parseLine(cmds[2]));
+	Filter filter2 = filterConverter.getFilter(parser.parseLine(cmds[2]));
+	EXPECT_EQ(1, delCommand1.deleteByName(db, filter2));
 }
 
 TEST_F(DeleteTest, TestBirth)
 {
 	vector<string>cmds;
-	cmds.emplace_back("DEL, , , ,birth, 20200131");
-	cmds.emplace_back("DEL, , , ,birth, 20200101");
-	cmds.emplace_back("DEL,-p,-y, ,birth, 2020");
-	cmds.emplace_back("DEL,-p,-m, ,birth, 01");
-	cmds.emplace_back("DEL,-p,-d, ,birth, 01");
+	cmds.emplace_back("DEL,-p,-y, ,birthday,2001");
+	cmds.emplace_back("DEL, , , ,birthday,20000101");
+	cmds.emplace_back("DEL,-p,-d, ,birthday,01");
+	cmds.emplace_back("DEL,-p,-m, ,birthday,02");
 
+	// DB[1], DB[2] deleted
+	Del delCommand0(parser.parseLine(cmds[0]));
+	Filter filter0 = filterConverter.getFilter(parser.parseLine(cmds[0]));
+	EXPECT_EQ(2, delCommand0.deleteByBirth(db, filter0));
 
-	// Return the number of deleted data
-	//delCommand = new Del(parser.parseLine(cmds[0]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(NONE, deleteByBirth(cmd));
+	// DB[0] deleted
+	Del delCommand1(parser.parseLine(cmds[1]));
+	Filter filter1 = filterConverter.getFilter(parser.parseLine(cmds[1]));
+	EXPECT_EQ(1, delCommand1.deleteByBirth(db, filter1));
 
-	//delCommand = new Del(parser.parseLine(cmds[1]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	//	EXPECT_EQ(11, deleteByBirth(cmd_b));
+	for (int i = 0; i < 3; i++)
+		db.insert(Database[i]);
+	// DB[0], DB[2] deleted
+	Del delCommand2(parser.parseLine(cmds[2]));
+	Filter filter2 = filterConverter.getFilter(parser.parseLine(cmds[2]));
+	EXPECT_EQ(2, delCommand1.deleteByBirth(db, filter2));
 
-	// Return the set of delete data which has parameter's name
-	//delCommand = new Del(parser.parseLine(cmds[2]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_y;
-	//	EXPECT_EQ(result_y, deleteByBirth(cmd_y));
-
-	//delCommand = new Del(parser.parseLine(cmds[3]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_m;
-	//	EXPECT_EQ(result_m, deleteByBirth(cmd_m));
-
-	//delCommand = new Del(parser.parseLine(cmds[4]));
-	//EXPECT_TRUE(delCommand->execute(database, logger), 1);
-	vector<string>result_d;
-	//	EXPECT_EQ(result_d, deleteByBirth(cmd_d));
+	// DB[1] deleted
+	Del delCommand3(parser.parseLine(cmds[3]));
+	Filter filter3 = filterConverter.getFilter(parser.parseLine(cmds[3]));
+	EXPECT_EQ(1, delCommand1.deleteByBirth(db, filter3));
 }
 
