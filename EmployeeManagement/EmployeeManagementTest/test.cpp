a9c176fc4ee51167c5c54d64c1c8cb92bf74ce7f
Sungju Huh <sjslush@gmail.com>
2022-04-05 11:12:20 +0900

    [test] add 100K full scenario test
    Signed-off-by: Sungju Huh <sjslush@gmail.com>

--- EmployeeManagement/EmployeeManagementTest/test.cpp
+++ EmployeeManagement/EmployeeManagementTest/test.cpp
@@ -17,7 +17,7 @@ static bool checkFileEQ(FileIO& fileDb, FileIO& fileQuery) {
 	return true;
 }
 
-TEST(TestCaseName, FullScenarioTest) {
+TEST(FullScenarioTest, TC1) {
 	FileIO inputFile("../EmployeeManagement/input_20_20.txt", FileType::INPUT);
 	FileIO outputFile("../EmployeeManagement/result.txt", FileType::OUTPUT);
 	inputFile.open();
@@ -45,6 +45,48 @@ TEST(TestCaseName, FullScenarioTest) {
 
 	FileIO dbFile("../EmployeeManagement/output_20_20.txt", FileType::INPUT);
 	FileIO queryFile("../EmployeeManagement/result.txt", FileType::INPUT);
+	dbFile.open();
+	queryFile.open();
+	bool ret = checkFileEQ(dbFile, queryFile);
+	dbFile.close();
+	queryFile.close();
+
+	EXPECT_EQ(true, ret);
+}
+
+TEST(FullScenarioTest, 100KTC) {
+	FileIO outputFile("../EmployeeManagement/result100K.txt", FileType::OUTPUT);
+	outputFile.open();
+
+	Parser parser;
+	MemoryDatabase memDB;
+	IDatabase& DB = memDB;
+	FileLogger fileLogger(outputFile);
+	ILogger& logger = fileLogger;
+
+	CommandFactory commandFactory;
+	vector<string> inputString;
+
+	for (int i = 100000; i > 0; i--) {
+		string str = "ADD, , , ," + to_string(10000000 + i) + ",SAMSUNG KIM,CL4,010-1234-5678,20000101,PRO";
+		inputString.emplace_back(str);
+	}
+	inputString.emplace_back("DEL, , , ,employeeNum,18115040");
+	inputString.emplace_back("SCH, , , ,certi,PRO");
+	inputString.emplace_back("SCH,-p, , ,cl,CL4");
+	inputString.emplace_back("DEL, ,-l, ,name,KIM");
+	for (int i = 0; i < (100000 + 4); i++) {
+		string inputLine = inputString[i];
+		Input input = parser.parseLine(inputLine);
+		logger.setPrintDetail(input.getFirstOption() == "-p");
+		shared_ptr<ICommand> cmd = commandFactory.createCommand(input);
+		cmd->execute(DB, logger);
+	}
+
+	outputFile.close();
+
+	FileIO dbFile("../EmployeeManagement/output100K.txt", FileType::INPUT);
+	FileIO queryFile("../EmployeeManagement/result100K.txt", FileType::INPUT);
 	dbFile.open();
 	queryFile.open();
 	bool ret = checkFileEQ(dbFile, queryFile);
