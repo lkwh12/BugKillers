b356b79bf98739a5531524df417db42d3e802860
minje <minje46@gmail.com>
2022-04-05 14:22:44 +0900

    [refacotry] modify exception code

--- EmployeeManagement/EmployeeManagement/Del.h
+++ EmployeeManagement/EmployeeManagement/Del.h
@@ -3,13 +3,15 @@
 #include "ICommand.h"
 #include "FilterConverter.h"
 #include "Input.h"
-
-using namespace std;
+#include "Add.h"
 
 class Del : public ICommand {
 public:
 	Del(const Input& input_) : input(input_) {}
 	virtual bool execute(IDatabase& db, ILogger& logger) override {
+		if (!checkException()) {
+			return false;
+		}
 		auto result = db.query(getFilter());
 	
 		logger.dump(input.getCommand(), result);
@@ -29,6 +31,33 @@ public:
 private:
 	Input input;
 	FilterConverter filterConverter;
+
+	bool checkException()
+	{
+		const vector<string>& payloads = input.getPayload();
+		
+		if (payloads[0]=="employeeNum" && payloads[1].length() != LengthEmpNo) {
+			return false;
+		}
+		else if (payloads[0] == "name" && payloads[1].length() > LengthName) {
+			return false;
+		}
+		else if (payloads[0] == "cl" && 
+			(payloads[1] != "CL1" && payloads[1] != "CL2" && payloads[1] != "CL3" && payloads[1] != "CL4")) {
+			return false;
+		}
+		else if (payloads[0] == "phoneNum" && payloads[1].length() != LengthPhoneNum) {
+			return false;
+		}
+		else if (payloads[0] == "birthday"&& payloads[1].length() != LengthBirthday) {
+			return false;
+		}
+		else if (payloads[0] == "certi" && 
+			(payloads[1] != "ADV" && payloads[1] != "PRO" && payloads[1] != "EX")) {
+			return false;
+		}
+		return true;
+	}
 
 	Filter getFilter()
 	{
