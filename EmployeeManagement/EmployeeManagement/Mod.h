2ad150b626e00fc58f48935227cbb71f46d4b5dc
yh0203kim <michaelkim9202@gmail.com>
2022-04-05 16:52:51 +0900

    [feature] Modify exception 추가

--- EmployeeManagement/EmployeeManagement/Mod.h
+++ EmployeeManagement/EmployeeManagement/Mod.h
@@ -9,6 +9,8 @@ using namespace std;
 
 #define QUEARY_RESULT vector<shared_ptr<Employee>>
 
+constexpr auto SEARCH_COLUMN_INDEX    = (0);
+constexpr auto SEARCH_CONTENTS_INDEX  = (1);
 constexpr auto MODIFY_COLUMN_INDEX    = (2);
 constexpr auto MODIFY_CONTENTS_INDEX  = (3);
 
@@ -20,20 +22,68 @@ constexpr auto MIDDLE_PHONE_INDEX     = (1);
 constexpr auto LAST_PHONE_INDEX       = (2);
 constexpr auto PHONE_DELIMITER        = ('-');
 
-constexpr auto BIRTHDAT_YEAR_START_INDEX    = (0);
-constexpr auto BIRTHDAT_MONTH_START_INDEX   = (4);
-constexpr auto BIRTHDAT_DAY_START_INDEX     = (6);
-constexpr auto BIRTHDAT_YEAR_START_SIZE     = (4);
-constexpr auto BIRTHDAT_MONTH_START_SIZE    = (2);
-constexpr auto BIRTHDAT_DAY_START_SIZE      = (2);
+constexpr auto BIRTHDAY_YEAR_START_INDEX    = (0);
+constexpr auto BIRTHDAY_MONTH_START_INDEX   = (4);
+constexpr auto BIRTHDAY_DAY_START_INDEX     = (6);
+constexpr auto BIRTHDAY_YEAR_START_SIZE     = (4);
+constexpr auto BIRTHDAY_MONTH_START_SIZE    = (2);
+constexpr auto BIRTHDAY_DAY_START_SIZE      = (2);
 
-#define GET_YEAR_FROM_BIRTH(birth)          (birth.substr(BIRTHDAT_YEAR_START_INDEX, BIRTHDAT_YEAR_START_SIZE))   
-#define GET_MONTH_FROM_BIRTH(birth)         (birth.substr(BIRTHDAT_MONTH_START_INDEX, BIRTHDAT_MONTH_START_SIZE))
-#define GET_DAY_FROM_BIRTH(birth)           (birth.substr(BIRTHDAT_DAY_START_INDEX, BIRTHDAT_DAY_START_SIZE))
+constexpr auto EMPLOYEENUM_LENGTH           = (8);
+constexpr auto NAME_CHUNK_SIZE              = (2);
+constexpr auto NAME_LENGTH                  = (15);
+constexpr auto PHONENUM_CHUNK_SIZE          = (3);
+constexpr auto PHONENUM_LENGTH              = (13);
+constexpr auto PHONENUM_MIDDLE_LENGTH       = (4);
+constexpr auto PHONENUM_LAST_LENGTH         = (4);
+constexpr auto BIRTHDAY_LENGTH              = (8);
+constexpr auto BIRTHDAY_YEAR_LENGTH         = (4);
+constexpr auto BIRTHDAY_MONTH_LENGTH        = (2);
+constexpr auto BIRTHDAY_DAY_LENGTH          = (2);
+
+
+#define GET_YEAR_FROM_BIRTH(birth)          (birth.substr(BIRTHDAY_YEAR_START_INDEX, BIRTHDAY_YEAR_START_SIZE))   
+#define GET_MONTH_FROM_BIRTH(birth)         (birth.substr(BIRTHDAY_MONTH_START_INDEX, BIRTHDAY_MONTH_START_SIZE))
+#define GET_DAY_FROM_BIRTH(birth)           (birth.substr(BIRTHDAY_DAY_START_INDEX, BIRTHDAY_DAY_START_SIZE))
 
 #define GET_FUNCTION_INDEX(modifyColumn)    (find(ColumnForFunction.begin(), ColumnForFunction.end(), modifyColumn) - ColumnForFunction.begin())
 
-typedef void MODIFY(QUEARY_RESULT &, const string &);
+#define VERIFY_COLUMN_EMPLOYEENUM(payloads, columnIndex, contentsIndex)     (payloads.at(columnIndex) == "employeeNum" && \
+                                                                             payloads.at(contentsIndex).length() != EMPLOYEENUM_LENGTH)
+#define VERIFY_COLUMN_NAME(payloads, columnIndex, contentsIndex)            (payloads.at(columnIndex) == "name" && \
+	                                                                         payloads.at(contentsIndex).length() > NAME_LENGTH)
+#define VERIFY_COLUMN_CL(payloads, columnIndex, contentsIndex)              (payloads.at(columnIndex) == "cl" && \
+                                                                                (payloads.at(contentsIndex) != "CL1" && \
+                                                                                 payloads.at(contentsIndex) != "CL2" && \
+                                                                                 payloads.at(contentsIndex) != "CL3" && \
+                                                                                 payloads.at(contentsIndex) != "CL4"))
+#define VERIFY_COLUMN_PHONENUM(payloads, columnIndex, contentsIndex)        (payloads.at(columnIndex) == "phoneNum" && \
+                                                                                 (payloads.at(contentsIndex).length() != PHONENUM_LENGTH && \
+                                                                                  payloads.at(contentsIndex).length() != PHONENUM_MIDDLE_LENGTH && \
+                                                                                  payloads.at(contentsIndex).length() != PHONENUM_LAST_LENGTH))
+#define VERIFY_COLUMN_BIRTHDAY(payloads, columnIndex, contentsIndex)        (payloads.at(columnIndex) == "birthday" && \
+                                                                                 (payloads.at(contentsIndex).length() != BIRTHDAY_LENGTH && \
+                                                                                  payloads.at(contentsIndex).length() != BIRTHDAY_YEAR_LENGTH && \
+                                                                                  payloads.at(contentsIndex).length() != BIRTHDAY_MONTH_LENGTH && \
+                                                                                  payloads.at(contentsIndex).length() != BIRTHDAY_DAY_LENGTH))
+#define VERIFY_COLUMN_CERTI(payloads, columnIndex, contentsIndex)           (payloads.at(columnIndex) == "certi" && \
+                                                                                (payloads.at(contentsIndex) != "ADV" && \
+                                                                                 payloads.at(contentsIndex) != "PRO" && \
+                                                                                 payloads.at(contentsIndex) != "EX"))
+
+#define VERIFY_SEARCH_COLUMN_EMPLOYEENUM(payloads)           VERIFY_COLUMN_EMPLOYEENUM(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+#define VERIFY_SEARCH_COLUMN_NAME(payloads)                  VERIFY_COLUMN_NAME(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+#define VERIFY_SEARCH_COLUMN_CL(payloads)                    VERIFY_COLUMN_CL(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+#define VERIFY_SEARCH_COLUMN_PHONENUM(payloads)              VERIFY_COLUMN_PHONENUM(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+#define VERIFY_SEARCH_COLUMN_BIRTHDAY(payloads)              VERIFY_COLUMN_BIRTHDAY(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+#define VERIFY_SEARCH_COLUMN_CERTI(payloads)                 VERIFY_COLUMN_CERTI(payloads, SEARCH_COLUMN_INDEX, SEARCH_CONTENTS_INDEX)
+
+#define VERIFY_MODIFY_COLUMN_EMPLOYEENUM(payloads)           VERIFY_COLUMN_EMPLOYEENUM(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
+#define VERIFY_MODIFY_COLUMN_NAME(payloads)                  VERIFY_COLUMN_NAME(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
+#define VERIFY_MODIFY_COLUMN_CL(payloads)                    VERIFY_COLUMN_CL(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
+#define VERIFY_MODIFY_COLUMN_PHONENUM(payloads)              VERIFY_COLUMN_PHONENUM(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
+#define VERIFY_MODIFY_COLUMN_BIRTHDAY(payloads)              VERIFY_COLUMN_BIRTHDAY(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
+#define VERIFY_MODIFY_COLUMN_CERTI(payloads)                 VERIFY_COLUMN_CERTI(payloads, MODIFY_COLUMN_INDEX, MODIFY_CONTENTS_INDEX)
 
 class Mod : public ICommand {
 public:
@@ -50,6 +100,10 @@ public:
 
 		Filter filter = filterConverter_.getFilter(input_);
 
+		if (!checkSearchExcetion()) {
+	        return false;
+        }
+
 		QUEARY_RESULT queryResult = db.query(filter);
 
 		logger.dump(input_.getCommand(), queryResult);
@@ -63,6 +117,11 @@ public:
 
 	bool doModify(QUEARY_RESULT& searchResult) {
 		vector<string> payload = input_.getPayload();
+		
+		if (!checkModifyExcetion()) {
+			return false;
+		}
+
 		string modifyColumn = payload.at(MODIFY_COLUMN_INDEX);
 
 		(this->*pModify[GET_FUNCTION_INDEX(modifyColumn)])(searchResult, payload.at(MODIFY_CONTENTS_INDEX));
@@ -93,6 +152,54 @@ private:
 	};
 
 	void (Mod::*pModify[(int)Mod::FunctionIndex::E_COLUMN_MAX_COUNT])(QUEARY_RESULT&, const string&);
+
+	bool checkSearchExcetion() {
+		const vector<string>& payloads = input_.getPayload();
+
+		if (VERIFY_SEARCH_COLUMN_EMPLOYEENUM(payloads)) {
+			return false;
+		}
+		else if (VERIFY_SEARCH_COLUMN_NAME(payloads)) {
+			return false;
+		}
+		else if (VERIFY_SEARCH_COLUMN_CL(payloads)) {
+			return false;
+		}
+		else if (VERIFY_SEARCH_COLUMN_PHONENUM(payloads)) {
+			return false;
+		}
+		else if (VERIFY_SEARCH_COLUMN_BIRTHDAY(payloads)) {
+			return false;
+		}
+		else if (VERIFY_SEARCH_COLUMN_CERTI(payloads)) {
+			return false;
+		}
+		return true;
+	}
+
+	bool checkModifyExcetion() {
+		const vector<string>& payloads = input_.getPayload();
+
+		if (VERIFY_MODIFY_COLUMN_EMPLOYEENUM(payloads)) {
+			return false;
+		}
+		else if (VERIFY_MODIFY_COLUMN_NAME(payloads)) {
+			return false;
+		}
+		else if (VERIFY_MODIFY_COLUMN_CL(payloads)) {
+			return false;
+		}
+		else if (VERIFY_MODIFY_COLUMN_PHONENUM(payloads)) {
+			return false;
+		}
+		else if (VERIFY_MODIFY_COLUMN_BIRTHDAY(payloads)) {
+			return false;
+		}
+		else if (VERIFY_MODIFY_COLUMN_CERTI(payloads)) {
+			return false;
+		}
+		return true;
+	}
 
 	void modifyEmployeeNum(QUEARY_RESULT& searchResult, const string& modifyContents) {
 		for (auto& Employee : searchResult) {
